7fde1d4f76bd2431731a7eecf8facb0734988533
georgiarood <51422481+georgiarood@users.noreply.github.com>
2020-08-11 11:19:03 -0400

    Add files via upload

---
+++ ToDoListPractice0/ToDoListPractice0/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListPractice0
+//
+//  Created by Apple on 8/11/20.
+//  Copyright © 2020 Georgia Rood. All rights reserved.
+//
+
+import UIKit
+class ToDoClass {
+    var description = ""
+    var important = false
+}
+
